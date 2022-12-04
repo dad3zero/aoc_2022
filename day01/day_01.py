4b343c6310f52539687c706489870aeeba425003
dad3zero <dad3zero@gmail.com>
2022-12-04 12:18:49 +0100

    Cleaned up day 1 code with a function extraction.

--- day01/day_01.py
+++ day01/day_01.py
@@ -6,17 +6,24 @@ This code was updated after the first question to answer both of them.
 
 src_file = "input.txt"
 
-elves_backpack = []
+
+def update_top_packs(top_packs: list, new_value: int):
+    if new_value > top_packs[0]:
+        top_packs[0] = new_value
+        top_packs.sort()
+
+
 current_pack = []
+top_carry = [0, 0, 0]
 
 with open(src_file) as elves_input:
     for line in elves_input:
         if line.strip():
             current_pack.append(int(line))
         else:
-            elves_backpack.append(current_pack)
+            carry = sum(current_pack)
             current_pack = []
+            update_top_packs(top_carry, carry)
 
-top = sorted([sum(element) for element in elves_backpack])[-3:]
-print("Max value:", top[-1])
-print("Top 3:", top, sum(top))
+print("Max value:", top_carry[-1])
+print("Top 3:", top_carry, sum(top_carry))
