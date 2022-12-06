95155468b3367caacec4972cdc478b3a9e1ea09b
dad3zero <dad3zero@gmail.com>
2022-12-06 23:16:49 +0100

    Added a version using deque in day06

--- day06/day_06.py
+++ day06/day_06.py
@@ -1,3 +1,4 @@
+from collections import deque
 from utils import io
 
 def _find_start_marker(seq:str, window_size: int):
@@ -6,6 +7,15 @@ def _find_start_marker(seq:str, window_size: int):
         if len(set(seq[index-window_size_index:index+1])) == window_size:
             return index +1
 
+def _dfind_start_marker(seq:str, window_size: int):
+    marker_frame = deque(maxlen=window_size)
+
+    for index, char in enumerate(seq):
+        marker_frame.append(char)
+        if len(set(marker_frame)) == window_size:
+            return index +1
+
+
 
 def find_start_index(seq):
     return _find_start_marker(seq, 4)
@@ -13,7 +23,8 @@ def find_start_index(seq):
 def find_start_message(seq):
     return _find_start_marker(seq, 14)
 
-stream = next(io.load_game_input())
-print(find_start_index(stream))
-stream = next(io.load_game_input())
-print(find_start_message(stream))
+if __name__ == "__main__":
+    stream = next(io.load_game_input())
+    print(find_start_index(stream))
+    stream = next(io.load_game_input())
+    print(find_start_message(stream))
